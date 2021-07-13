e75a4cdd09fbce0fbc269bceb86a795e04576a58
Abhilash <73437901+Katuriki@users.noreply.github.com>
2021-07-13 17:42:00 +0530

    Update othervars.py

--- assistant/othervars.py
+++ assistant/othervars.py
@@ -18,7 +18,7 @@ from . import *
 
 # --------------------------------------------------------------------#
 telegraph = Telegraph()
-r = telegraph.create_account(short_name="Ultroid")
+r = telegraph.create_account(short_name="Akira")
 auth_url = r["auth_url"]
 # --------------------------------------------------------------------#
 
